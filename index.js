2ff74f5d9d384846791b3e0bd52750c1f7459cba
samuel <mwausamuel33@gmail.com>
2025-12-24 00:45:52 +0300

    lab submission

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 // Function to change the background color when a button is clicked
 function changeBackgroundColor() {
   // Implement the function to change background color
+  document.body.style.backgroundColor = 'rgb(173, 216, 230)';
 }
 
 // Function to reset the background color when the body is double-clicked
 function resetBackgroundColor() {
   // Implement the function to reset background color
+  document.body.style.backgroundColor = ``;
 }
 
 // Capture Keyboard Input
@@ -15,6 +17,9 @@ function resetBackgroundColor() {
 // Function to display the key pressed by the user
 function displayKeyPress(event) {
   // Implement the function to display key pressed
+  const input = document.getElementById('textInput').value;
+  const display = document.getElementById('textInputDisplay');
+  display.textContent = `You typed: ${event.displayKeyPress}`;
 }
 
 // Process Text Input
@@ -22,6 +27,8 @@ function displayKeyPress(event) {
 // Function to display user input in real-time
 function displayUserInput() {
   // Implement the function to display user input
+  const display = document.getElementById('keyPressDisplay');
+  display.textContent = `Key pressed: ${input}`;
 }
 
 // Attach Event Listeners
@@ -36,11 +43,19 @@ function setupEventListeners() {
     .getElementById('resetColorButton')
     .addEventListener('dblclick', resetBackgroundColor)
 
+    function setupEventListeners () {
+      document.getElementById("changeColorButton").addEventListener('click',
+        changeBackgroundColor
+      );
+    }
+
   // Attach event listener to display key pressed when a key is pressed down
   document.addEventListener('keydown', displayKeyPress)
 
   // Attach event listener to display user input in real-time as they type
   document.getElementById('textInput').addEventListener('input', displayUserInput)
+  
+
 }
 
 // Initialize event listeners when the DOM is loaded
